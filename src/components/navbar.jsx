f884f2bc06d2617eb4c3e6d58688fc0d09df19a7
sadiquex <abubakasaddik1@gmail.com>
2022-11-26 21:12:24 +0000

    navbar components

---
+++ src/components/navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "../navbar.css";
+
+export default function Navbar() {
+  const [active, setActive] = useState("nav__menu");
+  // const [toggleIcon, setToggleIcon] = useState('nav_toggler')
+  
+  const navToggler = () => {
+    active === "nav__menu"
+      ? setActive("nav__menu nav__active")
+      : setActive("nav__menu");
+
+      // // toggleicon
+      // toggleIcon === 'nav__toggler' ? setToggleIcon('nav__toggler toggle'):
+      // setToggleIcon('nav__toggler')
+  };
+
+  return (
+    <nav className="nav">
+      <a href="#" className="nav__brand">
+        sadique
+      </a>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            About
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Skills
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Portfolio
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact
+          </a>
+        </li>
+      </ul>
+      <div className="nav_toggler" onClick={navToggler}>
+        <div className="line1 1"></div>
+        <div className="line2 2"></div>
+        <div className="line3 3"></div>
+      </div>
+    </nav>
+  );
+}
